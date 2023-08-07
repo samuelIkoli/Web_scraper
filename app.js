76a14ef741c9c7695d442aae721eb3630a921450
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-08-07 16:22:08 +0100

    vercel migration

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
